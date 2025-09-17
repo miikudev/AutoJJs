c95e6ac04b9d3cf7141dc1f832e767aec9893fb7
Zv_yz <zvyzemail@gmail.com>
2025-09-17 01:50:52 -0300

    feat: improved the chat message

--- Modules/RemoteChat.lua
+++ Modules/RemoteChat.lua
@@ -1,5 +1,6 @@
 local RemoteChat = {}
-local Channels = {}
+--local Channels = {}
+local Connections = {}
 
 local WC = game.WaitForChild
 local FFC = game.FindFirstChild
@@ -10,14 +11,19 @@ local TextChatService = game:GetService("TextChatService")
 local Player = Players.LocalPlayer
 local PlayerGui = Player:WaitForChild("PlayerGui", 95)
 
+local CurrentChannel: TextChannel;
+local InputBar = TextChatService:FindFirstChildOfClass("ChatInputBarConfiguration")
+
 local Methods = {
 	[Enum.ChatVersion.LegacyChatService] = function(Message: string)
 		local ChatUI = PlayerGui:FindFirstChild("Chat")
-
-		if ChatUI then
+		
+		if CurrentChannel then
+			CurrentChannel:SendAsync(Message)
+		elseif ChatUI then
 			local ChatFrame = WC(ChatUI, "Frame", 95)
 			local CBPF = WC(ChatFrame, "ChatBarParentFrame", 95)
-			
+
 			local Frame = WC(CBPF, "Frame", 95)
 			local BF = WC(Frame, "BoxFrame", 95)
 
@@ -27,13 +33,13 @@ local Methods = {
 			ChatBar:CaptureFocus()
 			ChatBar.Text = Message
 			ChatBar:ReleaseFocus(true)
-		else
-			Channels["RBXGeneral"]:SendAsync(Message)
 		end
 	end,
 
 	[Enum.ChatVersion.TextChatService] = function(Message: string)
-		Channels["RBXGeneral"]:SendAsync(Message)
+		if CurrentChannel then
+			CurrentChannel:SendAsync(Message)
+		end
 	end,
 }
 
@@ -41,9 +47,15 @@ function RemoteChat:Send(Message: string)
 	pcall(Methods[TextChatService.ChatVersion], Message)
 end
 
-for _, Channel in ipairs(TextChatService:GetDescendants()) do
-	if Channel:IsA("TextChannel") then
-		Channels[Channel.Name] = Channel
+if InputBar then
+	table.insert(Connections, InputBar.Changed:Connect(function(Prop: string)
+		if Prop == "TargetTextChannel" and (typeof(InputBar.TargetTextChannel) == "Instance" and InputBar.TargetTextChannel:IsA("TextChannel")) then
+			CurrentChannel = InputBar.TargetTextChannel
+		end
+	end))
+	
+	if typeof(InputBar.TargetTextChannel) == "Instance" and InputBar.TargetTextChannel:IsA("TextChannel") then
+		CurrentChannel = InputBar.TargetTextChannel
 	end
 end
 
